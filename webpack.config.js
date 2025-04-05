7cef7b46838d6b02a9b6bad237a5aa3a14494f88
Jeff Stanley <10554861+JS-Law@users.noreply.github.com>
2025-04-05 19:54:02 -0400

    Initialize project

---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  devServer: {
+    static: path.resolve(__dirname, "dist"),
+    port: 8080,
+    open: true,
+    hot: true,
+    compress: true,
+    liveReload: true,
+    watchFiles: ["src/**/*"],
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.scss$/i,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+  watchOptions: {
+    poll: 1000,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      inject: "body",
+      template: "./src/index.html",
+    }),
+  ],
+};
